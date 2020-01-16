0a742db33ebe39ca4302e9417ef7cc09bf8d9e3d
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-01-16 17:44:08 -0500

    added insertion sort (int)

--- Sort & Search/app.py
+++ Sort & Search/app.py
@@ -13,6 +13,8 @@ def linear_search(list=[], tar=str):
 
 linear_search(employees, 'kevin')
 
+# ===============================================================================================================================
+
 # Binary search - performed on simple list of int as binary search algorithms can only be ran against sorted list
 sorted_list = [1, 2, 3, 4, 5, 7, 10, 14, 17, 18, 20, 23, 25, 28, 31, 32, 34, 37, 38, 39, 42, 54, 55, 56, 57, 64, 74]
 
@@ -39,3 +41,21 @@ def binary_search(list=[], tar=int):
 
 
 binary_search(sorted_list, 25)
+
+# ===============================================================================================================================
+
+
+# Insertion Sort: better than bubble sort but not very efficient as un able to scale for larger data sets.
+
+unsorted_list = [4, -5,  7, 6, 8, 9, 556, -254, 45, 5, 6, 56, 5, 6, 34, 36, 43, 76, 65]
+
+def insertion_sort(list=[]):
+    for i in range(len(list)):
+        while list[i - 1] > list[i] and i > 0:  # i > 0 will make it crash while checking first item
+            # don't get confused, just optimized syntax to swap values
+            list[i], list[i-1] = list[i-1], list[i]
+            i = i-1  # important to check with previous item
+
+    return list
+
+print(insertion_sort(unsorted_list))
